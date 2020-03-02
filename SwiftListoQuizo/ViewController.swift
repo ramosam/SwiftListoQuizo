fad107eff664996a1be080dd811092a3a2e4aaf4
Alicia Ramos <ramosam@my.lanecc.edu>
2020-03-01 16:40:56 -0800

    Initial Commit

---
+++ SwiftListoQuizo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftListoQuizo
+//
+//  Created by Arcaurel on 3/1/20.
+//  Copyright © 2020 Ramosam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
